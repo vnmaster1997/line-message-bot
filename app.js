91e7f3b693e136636e73b2b8d0403b9ae0d49b76
Nam Nguyen <namnv537@gmail.com>
2019-12-02 12:57:18 +0700

    fix app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+app.get('/', (req, res) => res.send("hello"))
 app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
