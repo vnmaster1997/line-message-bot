3b5378099971136415c60d423ebd608f0134b4e7
Nam Nguyen <namnv537@gmail.com>
2019-12-02 13:58:09 +0700

    a

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 app.get('/', (req, res) => res.send("hello world"))
-app.post('/webhook', (req, res) => res.sendStatus(200))
+// app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
